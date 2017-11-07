317b8010bf6922abf43d8c778c5b2fa8631258b6
pvrba002c <phonkee@phonkee.eu>
2017-11-08 00:11:39 +0100

    Added tests for response

---
+++ response_test.go
@@ -0,0 +1,24 @@
+package rip
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"context"
+	"fmt"
+)
+
+func TestResponse(t *testing.T) {
+
+	Convey("Test New", t, func() {
+		Convey("Test Something", func() {
+			client := New("http://dog.ceo/api/").Get("breeds/list/all")
+			response := client.Do(context.Background())
+			So(fmt.Sprintf("%T", client), ShouldEqual, fmt.Sprintf("%T", response.Client()))
+
+			So(response.Header(), ShouldNotBeNil)
+			So(response.Do(context.Background()).Body(), ShouldResemble, response.Body())
+
+		})
+	})
+
+}
